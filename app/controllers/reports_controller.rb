e1eaab4b70427ba03777aee94b61d0b9b7e56fa0
Daniel Cisneros Medina <webmasterofpuppetz@gmail.com>
2021-01-17 22:28:58 -0600

    First version of the report view

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,5 +1,27 @@
+require 'date'
+
 class ReportsController < ApplicationController
   def daily
+    #get current date to find the current week
+    d = DateTime.now
+    today = Day.find_by(date_str: d.strftime("%d/%m/%Y"))
+    @days = Day.where(week_id: today.week.id).order(:date)
+    @dh = DailyHour.where(day: @days)
+    teams = Team.all.order(:team)
+    @result = {}
+    teams.each do |t|
+      @result[t.team] = {}
+      consultants = Consultant.where(team_id: t.id).order(:name)
+      consultants.each do |c|
+        @result[t.team][c.name] = {}
+        @days.each do |d|
+          @result[t.team][c.name][d.date_str] = 0
+        end
+      end
+    end
+    @dh.each do |dh|
+      @result[dh.consultant.team.team][dh.consultant.name][dh.day.date_str] += dh.hours
+    end
   end
 
   def weekly
--- app/models/consultant.rb
+++ app/models/consultant.rb
@@ -3,4 +3,5 @@ class Consultant < ApplicationRecord
   friendly_id :name, use: :slugged
   
   belongs_to :team
+  has_many :daily_hours
 end
